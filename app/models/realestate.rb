52afcfda807281e9c6016a1b59ea2e75e3f505f1
Guilherme Y. Hatano <guihatano@gmail.com>
2017-06-12 18:20:13 -0300

    adds enum category updates realestates view

--- app/models/realestate.rb
+++ app/models/realestate.rb
@@ -1,6 +1,7 @@
 class Realestate < ApplicationRecord
 	extend Enumerize
 	enumerize :re_type, in: [:house, :apartment, :rural, :plot]
+	enumerize :category, in: [:rent, :sale, :both]
 
 	has_attached_file :img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :img, content_type: /\Aimage\/.*\z/
