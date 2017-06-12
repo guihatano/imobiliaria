ad2e54a67a339a73f0855ba7035877df0701a86b
Guilherme Y. Hatano <guihatano@gmail.com>
2017-06-12 10:43:32 -0300

    updates/implements i18n updates realestate views

--- app/models/realestate.rb
+++ app/models/realestate.rb
@@ -1,2 +1,8 @@
 class Realestate < ApplicationRecord
+
+	has_attached_file :img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :img, content_type: /\Aimage\/.*\z/
+
+  validates :re_type, :street, :district, :city, :state, :category, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
