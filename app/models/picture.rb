bc7a28faa92edaae267a0862acfc892482296d04
Guilherme Y. Hatano <guihatano@gmail.com>
2017-06-14 12:19:19 -0300

    fixes params images
    images can be null
    fixes some i18n

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,8 @@
 class Picture < ApplicationRecord
 	belongs_to :realestate
 
-   has_attached_file :image, 
-		styles: { medium: "300x300>", thumb: "100x100>" },
+  has_attached_file :image, 
+		styles: { medium: "x300>", thumb: "x100>" },
 		default_url: "/images/:style/missing.png"
 
   do_not_validate_attachment_file_type :image
