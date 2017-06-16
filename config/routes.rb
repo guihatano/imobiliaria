1214ca3335f968923bd18c4f7642c53729886c83
Guilherme Y. Hatano <guihatano@gmail.com>
2017-06-16 15:13:12 -0300

    adds support to remove image from pictures
    creates pictures_controller
    updates _form layout
    adds a route to pictures controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 	root 'realestates#index'
 	
   resources :realestates
+  delete '/realestates/:realestate_id/pictures/:id', to: 'pictures#destroy', as: :destroy_realestate_picture
   devise_for :users
 end
