73e5a75e0ebb3cfa07554fdad05471cddf1a991e
Guilherme Y. Hatano <guihatano@gmail.com>
2017-07-19 14:27:38 -0300

    adds lib to create images with watermark
    watermark image should be in app/assets/images/ as logo.png
    by default I generated one for myself.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,15 @@ class Picture < ApplicationRecord
 	belongs_to :realestate
 
   has_attached_file :image, 
-		styles: { medium: "x200>", thumb: "x100>" },
+		styles: { medium: "x200>", thumb: "x100>" , 
+			:original => {
+				:processors => [:watermark],
+				:watermark_path => "#{Rails.root}/app/assets/images/logo.png",
+				:geometry => '800>',
+				:position => 'SouthEast',
+				:watermark_dissolve => 70,
+				:auto_orient => false
+			}},
 		default_url: "/images/:style/missing.png"
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
